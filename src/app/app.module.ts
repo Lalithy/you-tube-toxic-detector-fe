7caf0d0d82af9ccf2685f728820d09f742a25c67
Lalithya <lalithyanawanjana@gmail.com>
2024-03-17 01:21:31 +0530

    Create Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InformationPageComponent } from './Home/information-page/information-page.component';
 import { MainPageComponent } from './Home/main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCommonModule} from '@angular/material/core';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +25,17 @@ import { MainPageComponent } from './Home/main-page/main-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCommonModule,
+    MatChipsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    NgxYoutubePlayerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
