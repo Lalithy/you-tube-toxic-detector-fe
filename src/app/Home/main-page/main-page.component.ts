f6d72fbb335941534b13a745ef3760fe115a14d6
Lalithya <lalithyanawanjana@gmail.com>
2024-03-17 13:06:26 +0530

    Get YouTube Player

--- src/app/Home/main-page/main-page.component.ts
+++ src/app/Home/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class MainPageComponent {
 
+  videoId: string = '';
+
+  ngOnInit() {
+    const scripTag = document.createElement('script')
+    scripTag.src = "https://www.youtube.com/iframe_api";
+    document.body.appendChild(scripTag);
+  }
+
+  extractVideoId(url: string): string {
+
+    const regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})$/;
+    const match = url.match(regex);
+
+    if (match) {
+      return match[1];
+    } else {
+
+      return '';
+    }
+  }
+
+  setYouTubeValues(url: string) {
+    this.videoId = this.extractVideoId(url);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InformationPageComponent } from './Home/information-page/information-page.component';
-import { MainPageComponent } from './Home/main-page/main-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {InformationPageComponent} from './Home/information-page/information-page.component';
+import {MainPageComponent} from './Home/main-page/main-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCommonModule} from '@angular/material/core';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatGridListModule} from '@angular/material/grid-list';
@@ -13,7 +13,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import {YouTubePlayer, YouTubePlayerModule} from '@angular/youtube-player';
 
 
 @NgModule({
@@ -32,10 +32,12 @@ import {MatButtonModule} from '@angular/material/button';
     MatCardModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    YouTubePlayerModule
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
